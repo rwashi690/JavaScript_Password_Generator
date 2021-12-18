302ade75c9f3762ceaf81704ed771a3995ccdf65
rwashi690 <rwashington325@gmail.com>
2021-12-17 21:03:24 -0700

    added banks for letters, numbers, and characters to generate a password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// First create a bank of letters, numbers, and special characters for the password
+letters = ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']
+numbers =['1234567890']
+characters =['@!#$%&']
+
+//Create variables for prompts
+var wantPassword = prompt('Would you like to generate a password? (yes/y or no/n)')
+var passwordlength = prompt('How long would you like the password to be? (enter a number less than 50)')
+
+
+function generatePassword(digits){
+  var digits
+  if( wantPassword == 'yes' && want){
+
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
